d92377d86ae0803554adb0ebd3f88e9863aefbb2
Pedro Soares <60124144+pedrinho-soares@users.noreply.github.com>
2025-04-29 15:43:07 -0300

    Comparador de números
    Compara e fala se um número é maior que outro:

---
+++ 1 - Comece por aqui/comparador_numeros.py
@@ -0,0 +1,11 @@
+numero1= float(input("Digite um número"))
+numero2= float(input("Digite um segundo número"))
+
+if numero1 > numero2:
+    print("O primeiro número é maior")
+
+elif numero2 > numero1:
+    print("O segundo número é maior")
+    
+else:
+    print("Os números são iguais")
